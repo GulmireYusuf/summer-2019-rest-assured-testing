d229be3682b5e66272d1d88a9c112b0a4f61458b
Gulmi Yusuf <gulmi199056@gmail.com>
2022-01-29 18:31:52 -0600

    base URI

--- src/test/java/tests/day2/LoggingExamples.java
+++ src/test/java/tests/day2/LoggingExamples.java
@@ -1,18 +1,24 @@
 package tests.day2;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 
 public class LoggingExamples {
+    @BeforeAll
+    public static void setUp(){
+        baseURI="http://ec2-18-208-250-149.compute-1.amazonaws.com:1000/ords/hr/";
 
+    }
     @Test
     public void requestLogging(){
         // see request logging; log.all-->prints everything in the request
         given().log().everything().
                 pathParam("id", "3").
-                when().get("http://ec2-18-208-250-149.compute-1.amazonaws.com:1000/ords/hr/regions/{id}").
+                when().get("/regions/{id}").
                 then().statusCode(200);
 
     }
@@ -22,8 +28,17 @@ public class LoggingExamples {
         //log().isStatusCodeIsEqualsTo(401/sth)-->prints if the status code matches the provided one
         //log().ifValidationFails-->prints if any assertion fails
         given().pathParam("id", "2").
-                when().get("http://ec2-18-208-250-149.compute-1.amazonaws.com:1000/ords/hr/regions/{id}").
+                when().get("/regions/{id}").
                 then().log().ifStatusCodeIsEqualTo(200).
+                statusCode(200);
+    }
+    @Test
+    public void test3(){
+        //both location we use log().everything()
+        given().log().everything().
+                pathParam("id", "2").
+                when().get("/regions/{id}").
+                then().log().everything().
                 statusCode(200);
     }
 }
