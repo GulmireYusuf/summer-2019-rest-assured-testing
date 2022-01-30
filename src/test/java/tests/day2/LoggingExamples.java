66f15ddb7197c59b1bc1fbeb515ea8ffdb9cbf78
Gulmi Yusuf <gulmi199056@gmail.com>
2022-01-29 18:39:55 -0600

    small update

--- src/test/java/tests/day2/LoggingExamples.java
+++ src/test/java/tests/day2/LoggingExamples.java
@@ -4,10 +4,15 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static io.restassured.RestAssured.*;
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 public class LoggingExamples {
+    //@BeforeAll-->runs before everything is this class.just like @BeforeClass from testNG.
+    //why make it static because Junit it runs like this. if we do not do static it will not run,
+    // or we add @TestInstance(Lifecycle.PER_CLASS) on the top(class top)
     @BeforeAll
     public static void setUp(){
         baseURI="http://ec2-18-208-250-149.compute-1.amazonaws.com:1000/ords/hr/";
