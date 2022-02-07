d74f8844b0211ea533eae574840e36c5ec8c6d0e
Gulmi Yusuf <gulmi199056@gmail.com>
2022-02-06 22:24:56 -0600

    Api key test

---
+++ src/test/java/tests/day7/ApiKeyTest.java
@@ -0,0 +1,32 @@
+package tests.day7;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiKeyTest {
+    /**
+     * test the 401 status code by not passing the api key
+     */
+    @Test
+    public void noKey(){
+        given().queryParam("t","Frozen").
+                when().get("http://www.omdbapi.com/").
+                prettyPeek().then().statusCode(401).
+                body("Error",equalTo("No API key provided."));
+
+    }
+    /**
+     * call the omdbapi with a valid api key
+     */
+    @Test
+    public void withKey(){
+        given().queryParam("apikey","e0484f01").
+                queryParam("t","Frozen").
+                when().get("http://www.omdbapi.com/").
+                prettyPeek().then().statusCode(200).
+                body("Title",equalTo("Frozen")).
+                body("Year",equalTo("2013"));
+    }
+}
