e241736c7e4ca8c92b3c039e9b790ed54c4bb6e7
Gulmi Yusuf <gulmi199056@gmail.com>
2022-02-09 14:18:42 -0600

    SSL validation

--- src/test/java/tests/day8/SSLExamples.java
+++ src/test/java/tests/day8/SSLExamples.java
@@ -8,16 +8,22 @@ import static io.restassured.RestAssured.when;
 public class SSLExamples {
 
     @Test
-    public void  badSSL(){               //
+    public void  badSSL(){
        given().relaxedHTTPSValidation(). //means we just try the server/ and go ahead
         when().get("https://untrusted-root.badssl.com/"). // website dosen't have a certificate
                 prettyPeek().then().statusCode(200);
     }
     @Test
     public void  useKeyStore(){
-        // in the given keyStore() in here we pass the location of the trust store file
+        // in the given trustStore() in here we pass the location of the trust store file and the password of the file
         given().
-                keyStore("/path/to/file","password").//this ketStore and password coming from developer or someone
+                //this path to file location of the file like (c://users/downloads/asdkdj.pdf) something like this (it takes string) and password comes from developer or someone
+                trustStore("/path/to/file","password").
+                when().get("").then().statusCode(200);
+
+        //in the given keyStore() in here we pass the location of the key store file and the password of the file
+        given().
+                keyStore("/path/to/file","password").//this path to file(it takes string) and password comes from developer or someone
                 when().get("").then().statusCode(200);
     }
 }
